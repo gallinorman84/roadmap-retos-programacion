90a5ea47f76c41a8548b8e5bae968fd3bce96754
gallinorman84 <gallioscar84@gmail.com>
2024-05-03 17:22:37 -0300

    Create normangalli84.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/normangalli84.py
@@ -0,0 +1,25 @@
+# https://python.org/
+
+#comentario en una linea
+
+"""
+esto es un cometario tambien
+"""
+
+'''
+esto es un cometario tambien
+'''
+
+my_variable = "mi variable"
+my_variable = "variable 2"
+
+MY_CONSTANT = "Mi constante" # por convencion
+
+my_int = 1
+my_float = 1.5
+my_bool = true
+my_bool = false
+my_string = "mi cadena de texto"
+my_string = "mi otra cadena de texto"
+
+print("hola, Norman"
